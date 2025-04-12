06c17fe1b682c692e421fb9e5e319f1c9921f765
RodrigoPereir4 <digolima167@gmail.com>
2025-04-12 19:48:10 -0300

    Projeto inicializado.

---
+++ src/main/java/com/example/twittersecurity/TwittersecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.twittersecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwittersecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwittersecurityApplication.class, args);
+	}
+
+}
